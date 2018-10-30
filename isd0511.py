3b33b873bede88db61d54a920d555f6ac1058f50
DJRockyFresh <43992131+DJRockyFresh@users.noreply.github.com>
2018-10-30 19:26:24 +0000

    Update isd0511.py

--- isd0511.py
+++ isd0511.py
@@ -1,15 +1,19 @@
 password = "changeme"
 userinput = input("Enter your password: ")
 tries = 1
+
 while userinput != password:
     print ("Wrong password, try again!")
     userinput = input("Enter your password: ")
     tries = tries + 1
-    
-    
+
     if userinput == password:
         print ("Welcome back!")
-        print (tries, "Attepmts were taken")
+    elif tries > 5:
+        print ("Access denied! Reset your password")
+        break
+print (tries, "Attepmts were taken")
+
 
 
     
