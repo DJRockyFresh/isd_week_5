305485e6d9d343e7f3bb12523416a0b252bb495d
DJRockyFresh <43992131+DJRockyFresh@users.noreply.github.com>
2018-10-30 19:11:19 +0000

    Update isd0505.py

--- isd0505.py
+++ isd0505.py
@@ -1,12 +1,2 @@
-grade = int(input("Enter your grade: "))
-if grade >=90:
-    letterGrade = "A"
-elif grade >=80:
-    letterGrade = "B"
-elif grade >=70:
-    letterGrade = "C"
-elif grade >=60:
-    letterGrade = "D"
-elif grade >=50:
-    letterGrade = "F"
-print("Your grade letter is ", letterGrade)
+mynum = int(input("Enter an integer number:"))
+print ((mynum / 7))
